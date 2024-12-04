e1ef5c09a954838e9e35821228fab5e43083b326
Bhabishworgrg <bhabishworgrg@gmail.com>
2024-12-04 22:30:46 +0545

    Implement AppCanvas methods

--- ASEAssignment/AppCanvas.cs
+++ ASEAssignment/AppCanvas.cs
@@ -2,65 +2,133 @@
 
 namespace ASEAssignment
 {
-    internal class AppCanvas : ICanvas
+    public class AppCanvas : ICanvas
     {
-        public int Xpos { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int Ypos { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public object PenColour { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    	private int xPos, yPos;
+		private int displayWidth, displayHeight;
+		private Color penColour;
+		private SolidBrush solidBrush;
+		private Pen pen;
+		private Font font;
+		private Bitmap bitmap;
+		private Graphics graphics;
+
+		public int Xpos
+		{ 
+			get => xPos; 
+			set => xPos = value;
+		}
+
+		public int Ypos
+		{
+			get => yPos;
+			set => yPos = value;
+		}
+
+        public object PenColour
+		{ 
+			get => penColour;
+			set
+			{
+				penColour = (Color)value;
+				solidBrush.Color = penColour;
+				pen.Color = penColour;
+			}
+		}
+
+		public AppCanvas()
+		{
+			xPos = yPos = 0;
+
+			displayWidth = 640;
+			displayHeight = 480;
+			
+			penColour = Color.Black;
+			solidBrush = new SolidBrush(penColour);
+			pen = new Pen(penColour);
+			font = new Font("Arial", 12);
+
+			bitmap = new Bitmap(displayWidth, displayHeight);
+			graphics = Graphics.FromImage(bitmap);
+		}
 
         public void Circle(int radius, bool filled)
         {
-            throw new NotImplementedException();
+			if (filled)
+			{
+				graphics.FillEllipse(solidBrush, xPos, yPos, radius, radius);
+			}
+			else
+			{
+				graphics.DrawEllipse(pen, xPos, yPos, radius, radius);
+			}
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+			graphics.Clear(Color.White);
         }
 
         public void DrawTo(int x, int y)
         {
-            throw new NotImplementedException();
+			graphics.DrawLine(pen, xPos, yPos, x, y);
+			xPos = x;
+			yPos = y;
         }
 
         public object getBitmap()
         {
-            throw new NotImplementedException();
+			return bitmap;
         }
 
         public void MoveTo(int x, int y)
         {
-            throw new NotImplementedException();
+			xPos = x;
+			yPos = y;
         }
 
         public void Rect(int width, int height, bool filled)
         {
-            throw new NotImplementedException();
+			if (filled)
+			{
+				graphics.FillRectangle(solidBrush, xPos, yPos, width, height);
+			}
+			else
+			{
+				graphics.DrawRectangle(pen, xPos, yPos, width, height);
+			}
         }
 
         public void Reset()
         {
-            throw new NotImplementedException();
-        }
+			xPos = yPos = 0;
+        	PenColour = Color.Black;
+		}
 
         public void Set(int width, int height)
         {
-            throw new NotImplementedException();
+			displayWidth = width;
+			displayHeight = height;
         }
 
         public void SetColour(int red, int green, int blue)
         {
-            throw new NotImplementedException();
+			PenColour = Color.FromArgb(red, green, blue);
         }
 
         public void Tri(int width, int height)
         {
-            throw new NotImplementedException();
+			Point[] points = new Point[3];
+			points[0] = new Point((xPos + width) / 2, yPos);
+			points[1] = new Point(xPos, yPos + height);
+			points[2] = new Point(xPos + width, yPos + height);
+
+			graphics.FillPolygon(solidBrush, points);
         }
 
         public void WriteText(string text)
         {
-            throw new NotImplementedException();
+			graphics.DrawString(text, font, solidBrush, xPos, yPos);
         }
     }
 }
