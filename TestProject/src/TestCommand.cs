3a21a79d3bf1e2a1074134a0ac8c56b8c954e8c9
Bhabishworgrg <bhabishworgrg@gmail.com>
2024-12-30 21:54:39 +0545

    Create test for multiline program

---
+++ TestProject/src/TestCommand.cs
@@ -0,0 +1,33 @@
+﻿using MainProject;
+using BOOSE;
+using System.Drawing;
+
+namespace TestProject
+{
+    [TestClass]
+    public sealed class TestCommand
+    {
+        private MainForm mainForm;
+
+        [TestInitialize]
+        public void TestInitialize() => mainForm = new MainForm();
+
+        [TestMethod]
+        [DataRow("circle 50\nmoveto 0,100\nrect 100, 200")]
+        [DataRow("\ncircle 50\nmoveto 0,100\nrect 100, 200\n")]
+        public void TestMultilineCommands(string multilineCommands)
+        {
+            try
+            {
+                // Act
+                mainForm.ExecuteCommand(multilineCommands);
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
+
